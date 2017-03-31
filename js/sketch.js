98c49acd07c609f7db588206b3bf71d035ed13d1
kq2137 <kq2137@columbia.edu>
2017-03-30 22:55:46 -0400

    “message”

--- js/sketch.js
+++ js/sketch.js
@@ -1,183 +1,82 @@
-// ***** Global variables ***** //
-var refugeeTable;
-var topRefugeesTable = new p5.Table;
-var maxTotal = 0;
-var maxLabel = 0;
-var maxLength = 500;
-var headers = ['Country','Refugees','Asylum-seekers','Returned refugees','IDPs','Returned IDPs','Stateless','Others of concern','Total']
-var startChartY = 100;
-var selectedButton = 8;
+var weatherData;
+var apiKey = 'a4f28a6a1aee327802340ba6b7c7dc2c';
+var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var city = 'Sydney';
+var units = 'metric';
+var maxTemp = 40;
+var minTemp = -5;
+var maxColor = 360;
+var minColor = 240;
+var currentTemp;
+var currentHumidity;
+var currentP;
+var button;
+var cityInput;
 
-// ***** Preload function ***** //
-function preload(){
-    refugeeTable = loadTable('../data/RefugeesUNHCR.csv', 'csv', 'header');
-    console.log('Done loading table...');
-}
+var dimensionX = 6;
+var dimensionY = 50;
+
 
 // ***** Setup function ***** //
 function setup(){
-    createCanvas(1000, 3000);
-    textSize(12);
-    console.log('Setup complete...');
-    print(refugeeTable.getRowCount() + ' rows loaded...');
-    print(refugeeTable.getColumnCount() + ' columns loaded...');
-    for (var i = 0; i < refugeeTable.getRowCount(); i++) {
-        maxTotal = max(refugeeTable.getNum(i, 'Total'), maxTotal);
-        maxLabel = max(refugeeTable.getString(i, 'Country').length, maxLabel);
-    }
-    print('Maximum total is ' + maxTotal);
-    print('Maximum label length is ' + maxLabel);
-    createNewTable();
+    createCanvas(1100, 800);
+    colorMode(RGB);
+    button = select('#submit');
+    cityInput = select('#city');
+    button.mousePressed(queryAPI);
 }
 
-// ****** Create new table function ******** //
-function createNewTable(){
-    for (var i = 0; i < headers.length; i++) {
-        topRefugeesTable.addColumn(headers[i]);
-    }
-    for (var i = 0; i < refugeeTable.getRowCount(); i++) {
-        var totalRefugees = refugeeTable.getNum(i, 'Total');
-        if (totalRefugees >= 100000) {
-            var newRow = topRefugeesTable.addRow()
-                        for (var j = 0; j < headers.length; j++) {
-                            newRow.setString(headers[j], refugeeTable.getString(i, headers[j]));
-                        }
-        }
-    }
-    print('New top refugee table created...');
-        print(topRefugeesTable);
+function queryAPI(){
+  var request = baseURL + cityInput.value() + '&units=' + units + '&apikey=' + apiKey;
+  loadJSON(request, getWeatherData);
 }
 
-function drawButtons(){
-    for (var i = 0; i < headers.length; i++){
-        if (selectedButton = i){
-            fill(150,150, 150);
-        }
-        else {
-            noFill();
-        }
-        noFill();
-        stroke(0);
-        strokeWeight(1);
-        rect(50 + 80 * i, 50, 75, 20);
-        fill(0);
-        noStroke();
-        textAlign(CENTER, CENTER);
-        textSize(8);
-        text(headers[i], 90 + 80 * i, 60);
-    }
+function getWeatherData(apiData){
+  weatherData = apiData;
+  currentTemp = weatherData.main.temp;
+  maxT = weatherData.main.temp_max;
+  minT = weatherData.main.temp_min;
+  currentHumidity = weatherData.main.humidity;
+  currentP = weatherData.main.pressure;
+  city = cityInput.value();
+  // console.log(weatherData.main.temp);
 }
 
-function mousePressed(){
-    if (mouseX > 130 && mouseX < 205 && mouseY > 50 && mouseY < 70){
-        selectedButton = 1;
-    }
-    if (mouseX > 210 && mouseX < 285 && mouseY > 50 && mouseY < 70){
-        selectedButton = 2;
-    }
-
-    if (mouseX > 290 && mouseX < 365 && mouseY > 50 && mouseY < 70){
-        selectedButton = 3;
-    }
-    if (mouseX > 370 && mouseX < 445 && mouseY > 50 && mouseY < 70){
-        selectedButton = 4;
-    }
-    if (mouseX > 450 && mouseX < 525 && mouseY > 50 && mouseY < 70){
-        selectedButton = 5;
-    }
-    if (mouseX > 530 && mouseX < 605 && mouseY > 50 && mouseY < 70){
-        selectedButton = 6;
-    }
-    if (mouseX > 610 && mouseX < 685 && mouseY > 50 && mouseY < 70){
-        selectedButton = 7;
-    }
-    if (mouseX > 690 && mouseX < 765 && mouseY > 50 && mouseY < 70){
-        selectedButton = 8;
-    }
-}
-
-function mousePosition(){
-  if (mouseX > 130 && mouseX < 205 && mouseY > 50 && mouseY < 70){
-    cursor(HAND);
-  }
-  else if (mouseX > 210 && mouseX < 285 && mouseY > 50 && mouseY < 70){
-    cursor(HAND);
-  }
-  else if (mouseX > 290 && mouseX < 365 && mouseY > 50 && mouseY < 70){
-    cursor(HAND);
-  }
-  else if (mouseX > 370 && mouseX < 445 && mouseY > 50 && mouseY < 70){
-    cursor(HAND);
-  }
-  else if (mouseX > 450 && mouseX < 525 && mouseY > 50 && mouseY < 70){
-    cursor(HAND);
-  }
-  else if (mouseX > 530 && mouseX < 605 && mouseY > 50 && mouseY < 70){
-    cursor(HAND);
-  }
-  else if (mouseX > 610 && mouseX < 685 && mouseY > 50 && mouseY < 70){
-    cursor(HAND);
-  }
-  else if (mouseX > 690 && mouseX < 765 && mouseY > 50 && mouseY < 70){
-    cursor(HAND);
-  }
-  else{
-    cursor(ARROW);
+function drawRect(){
+   for (var i = 0; i < dimensionX; i++) {
+      stroke(255);
+      strokeWeight(1);
+      fill(240);
+      rect(100, 60+(i*2)*20 ,1100, 20);
   }
 }
 
-function drawCountries(category){
-    fill(0);
-    noStroke();
-    textAlign(LEFT, TOP);
-    maxTotal = 0;
-    for (var i = 0; i < topRefugeesTable.getRowCount(); i++){
-        maxTotal = max(topRefugeesTable.getNum(i, category), maxTotal);
-    }
-    for (var i = 0; i < topRefugeesTable.getRowCount(); i++) {
-        var total = topRefugeesTable.getNum(i, category);
-        var length = map(total, 0, maxTotal, 0, maxLength);
-        rect(maxLabel * 5, startChartY + 2 + 14*i, length, 12);
-        text(nfc(total, 0), maxLabel * 5 + length + 5, startChartY + 14*i);
-    }
-    textAlign(RIGHT, TOP);
-    for (var i = 0; i < topRefugeesTable.getRowCount(); i++) {
-        text(topRefugeesTable.getString(i, 'Country'), maxLabel * 5 - 5, startChartY +14*i);
-    }
-}
-
-function showDetails(){
-    yPos(mouseY > 101)
-    var selectedRow = floor((y - 100) / 14);
-    text(topRefugeesTable.getString(selectedRow, 'Country'), 600, 120);
-    text(topRefugeesTable.getNum(selectedRow, 'Refugees'),600, 150);
-    text(topRefugeesTable.getNum(selectedRow, 'Asylum-seekers'), 600, 180);
-}
-
-function drawCountryDetails(xPos, yPos){
-    textAlign(LEFT, TOP);
-  fill(0);
-    if (yPos > 105 && yPos < 885){
-        var selectedCountry = floor((yPos - 105) / 14);
-        textSize(24);
-        text(topRefugeesTable.getString(selectedCountry, 'Country'), 750, 105);
-        textSize(12);
-        text('Refugees: ' + nfc(topRefugeesTable.getNum(selectedCountry, 'Refugees'), 0), 750, 135);
-        text('IDPs: ' + nfc(topRefugeesTable.getNum(selectedCountry, 'IDPs'), 0), 750, 150);
-        text('Stateless: ' + nfc(topRefugeesTable.getNum(selectedCountry, 'Stateless'), 0), 750, 165);
-        text('Total: ' + nfc(topRefugeesTable.getNum(selectedCountry, 'Total'), 0), 750, 180);
-    }
-}
 
 // ***** Draw function ***** //
 function draw(){
     background(255);
-    // drawCountries(refugeeTable);
-    drawCountries(headers[6]);
-    drawButtons();
-    mousePosition();
-    drawCountryDetails(mouseX, mouseY);
-    noStroke();
-    fill(0);
-    text(str(mouseX) + ', ' + str(round(mouseY)), mouseX, mouseY);
+    drawRect();
+    if (weatherData) {
+      noStroke();
+      fill(0);
+      text('                    Temperature in ' + city + ' : ' + str(currentTemp) + ' C', 20, 340);
+      text('                    Humidity in ' + city + ' : ' + str(currentHumidity) + '%', 20, 360);
+      text('                    Pressure in ' + city + ' : ' + str(currentP) + '%', 20, 380);
+      text('0 C', 100, 55);
+      text('10 C', 400, 55);
+      text('20 C', 700, 55);
+      text('30 C', 1000, 55);
+      text('990%', 60, 65);
+      text('1040%', 60, 290);
+      stroke(270-currentHumidity, 150+currentHumidity, 20);
+      fill(255);
+      strokeWeight(3);
+      rect(100+ minT*30 , 100, (maxT-minT)*30, (currentP-1000)*5);
+      strokeWeight(5);
+      line(100+currentTemp*30, 100, 100+currentTemp*30, 100+(currentP-1000)*5);
+    }
+    else{
+      text('Loading...', 20, 20);
+    }
 }
+
---
+++ js/wData.js
@@ -0,0 +1,29 @@
+// ***** Global variables ***** //
+var weatherData;
+var apiKey = 'a4f28a6a1aee327802340ba6b7c7dc2c';
+var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var city = 'New York';
+var units = 'metric';
+
+// ***** Setup function ***** //
+function setup(){
+    createCanvas(800, 800);
+    var request = baseURL + city + '&units=' + units + '&apikey=' + apiKey;
+    loadJSON(request, getWeatherData);
+}
+
+function getWeatherData(apiData){
+  weatherData = apiData;
+  console.log(weatherData.main);
+}
+
+// ***** Draw function ***** //
+function draw(){
+    background(255);
+    if (weatherData) {
+      text(weatherData.main.temp, 20, 20);
+    }
+    else{
+      text('Loading...', 20, 20);
+    }
+}
